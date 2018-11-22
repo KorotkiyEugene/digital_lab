4f313fceb911453dc7f5780091daab1e3fa1d5aa
Korotkiy <korotkiy.eugene@gmail.com>
2018-11-23 00:40:56 +0200

    Adding missed File... Ooops!

--- Lab4/hw/dec_7seg_demo_1/const_div.v
+++ Lab4/hw/dec_7seg_demo_1/const_div.v
@@ -2,29 +2,27 @@ module const_div(i_clk, i_rst_n, o_clk);
 
 input           i_clk;
 input           i_rst_n;
-output  reg     o_clk;
+output          o_clk;
 
-parameter       DIV_BY      = 7;
-localparam      CNT_CONST   = DIV_BY - 1;
-localparam      N           = $clog2(DIV_BY);
+parameter       DIV_BY = 7;
+localparam      N = $clog2(DIV_BY);
 
 reg     [N-1:0] cnt;
 
-wire    preload_cnt = ~|cnt;
+wire    [N-1:0] cnt_max      = (DIV_BY-1);
+wire            sync_rst_cnt = (cnt_max== cnt);
 
-always @(posedge i_clk)
-    o_clk <= preload_cnt;
+assign o_clk = sync_rst_cnt;
 
 always @(posedge i_clk, negedge i_rst_n) begin
     if(~i_rst_n) begin
-        cnt <= CNT_CONST;
+        cnt <= 0;
     end else begin
-        if (preload_cnt)
-            cnt <= CNT_CONST;
+        if (sync_rst_cnt)
+            cnt <= 0;
         else
-            cnt <= cnt - 1'b1;
+            cnt <= cnt + 1'b1;
     end
 end
 
 endmodule
-
